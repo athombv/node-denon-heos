66acc10b6814e159a800a6039e5a236cea2e409c
Emile Nijssen <emile@athom.nl>
2019-04-24 15:11:40 +0200

    fixes

--- examples/control.js
+++ examples/control.js
@@ -47,52 +47,77 @@ async function onSpeaker( speaker ) {
 	console.log('playerGetPlayers', players);
 	
 	if( players.length < 1 )
-	  throw new Error('No players found');
-	 
-	 const [ player ] = players;
-	 const { pid } = player;
-	 
-	 const playerGetPlayerInfo = await speaker.playerGetPlayerInfo({ pid });
-	 console.log('playerGetPlayerInfo', playerGetPlayerInfo);
-	 
-	 const playerGetPlayState = await speaker.playerGetPlayState({ pid });
-	 console.log('playerGetPlayState', playerGetPlayState);
-	 
-	 const playerGetNowPlayingMedia = await speaker.playerGetNowPlayingMedia({ pid });
-	 console.log('playerGetNowPlayingMedia', playerGetNowPlayingMedia);
-	 
-	 const playerGetVolume = await speaker.playerGetVolume({ pid });
-	 console.log('playerGetVolume', playerGetVolume);
-	 
-	 const playerSetVolume = await speaker.playerSetVolume({
+     throw new Error('No players found');
+    
+    const player = players.find(player => {
+      return player.name === name;
+    });
+    if(!player)
+     throw new Error('Player Not Found');
+    
+    const { pid } = player;
+    
+    speaker.on('player_now_playing_changed', () => {
+      speaker.playerGetNowPlayingMedia({ pid })
+        .then(console.log)
+        .catch(console.error);
+    })
+    
+    const playerGetPlayerInfo = await speaker.playerGetPlayerInfo({ pid });
+    console.log('playerGetPlayerInfo', playerGetPlayerInfo);
+    
+    const playerGetPlayState = await speaker.playerGetPlayState({ pid });
+    console.log('playerGetPlayState', playerGetPlayState);
+    
+    const playerGetNowPlayingMedia = await speaker.playerGetNowPlayingMedia({ pid });
+    console.log('playerGetNowPlayingMedia', playerGetNowPlayingMedia);
+    
+    const playerGetVolume = await speaker.playerGetVolume({ pid });
+    console.log('playerGetVolume', playerGetVolume);
+    
+    const playerSetVolume = await speaker.playerSetVolume({
      pid,
      level: parseInt(playerGetVolume.level),
    });
-	 console.log('playerSetVolume', playerSetVolume);
-	 
-	 const playerGetMute = await speaker.playerGetMute({ pid });
-	 console.log('playerGetMute', playerGetMute);
-	 
-	 const playerSetMute = await speaker.playerSetMute({ pid, mute: true });
-	 console.log('playerSetMute', playerSetMute);
-	 
-	 const playerGetPlayMode = await speaker.playerGetPlayMode({ pid });
-	 console.log('playerGetPlayMode', playerGetPlayMode);
-	 
-	 const playerSetPlayMode = await speaker.playerSetPlayMode({
-  	 pid,
-  	 shuffle: true,
-	 });
-	 console.log('playerSetPlayMode', playerSetPlayMode);
-	 
-	 await wait(5000);
-	 await speaker.disconnect();
-	 
-	 await wait(5000);
-	 await speaker.connect();
-	 
-	 const playerGetPlayerInfo2 = await speaker.playerGetPlayerInfo({ pid });
-	 console.log('playerGetPlayerInfo2', playerGetPlayerInfo2);
+    console.log('playerSetVolume', playerSetVolume);
+    
+    const playerGetMute = await speaker.playerGetMute({ pid });
+    console.log('playerGetMute', playerGetMute);
+    
+    const playerSetMute = await speaker.playerSetMute({ pid, mute: true });
+    console.log('playerSetMute', playerSetMute);
+    
+    const playerGetPlayMode = await speaker.playerGetPlayMode({ pid });
+    console.log('playerGetPlayMode', playerGetPlayMode);
+    
+    const playerSetPlayMode = await speaker.playerSetPlayMode({
+      pid,
+      shuffle: true,
+    });
+    console.log('playerSetPlayMode', playerSetPlayMode);
+    
+    const browseGetMusicSources = await speaker.browseGetMusicSources();
+    console.log('browseGetMusicSources', browseGetMusicSources);
+    
+    const auxIn = browseGetMusicSources.find(source => {
+      return source.name === 'AUX Input';
+    });
+    
+    if( auxIn ) {    
+      const browsePlayAuxIn1 = await speaker.browsePlayAuxIn1({ pid });
+      console.log('browsePlayAuxIn1', browsePlayAuxIn1);
+    }
+    
+    if( false ) {
+      await wait(5000);
+      await speaker.disconnect();
+      
+      await wait(5000);
+      await speaker.connect();
+      
+      const playerGetPlayerInfo2 = await speaker.playerGetPlayerInfo({ pid });
+      console.log('playerGetPlayerInfo2', playerGetPlayerInfo2);
+    }
 }
 
 async function wait(timeout) {
